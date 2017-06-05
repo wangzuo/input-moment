d86430b997eb211765a6266cba4f0322d548e7eb
Wang Zuo <wzuoadjusted@gmail.com>
2017-06-05 11:14:14 +0800

    test lib build

--- __tests__/input-moment.js
+++ __tests__/input-moment.js
@@ -1,7 +1,7 @@
 import moment from 'moment';
 import React from 'react';
 import renderer from 'react-test-renderer';
-import InputMoment from '../src/input-moment';
+import InputMoment from '../';
 
 test('render', () => {
   const m = moment().year(2018).month(7).date(8).hours(8).minutes(8).seconds(8);
