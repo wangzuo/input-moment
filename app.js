d9d64ab959281e123b314337fa6748e021b8e9c8
Wang Zuo <wzuoadjusted@gmail.com>
2017-06-05 11:07:24 +0800

    update gh-pages 2017-06-05T03:07:24.491Z

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-require('../src/less/input-moment.less');
-require('./app.less');
+import '../src/less/input-moment.less';
+import './app.less';
 
-var moment = require('moment');
-var React = require('react');
-var ReactDOM = require('react-dom');
-var InputMoment = require('../src/input-moment');
-var packageJson = require('../package.json');
+import moment from 'moment';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import InputMoment from '../src/input-moment';
+import packageJson from '../package.json';
 
-var App = React.createClass({
-  displayName: 'App',
+class App extends Component {
+  state = {
+    m: moment()
+  };
 
-  getInitialState() {
-    return {
-      m: moment()
-    };
-  },
+  handleChange = m => {
+    this.setState({ m });
+  };
+
+  handleSave = () => {
+    console.log('saved', this.state.m.format('llll'));
+  };
 
   render() {
     return (
@@ -22,30 +26,18 @@ var App = React.createClass({
         <h1>{packageJson.name}</h1>
         <h2>{packageJson.description}</h2>
         <form>
-        <div className="input">
-          <input
-            type="text"
-            value={this.state.m.format('llll')}
-            readOnly
+          <div className="input">
+            <input type="text" value={this.state.m.format('llll')} readOnly />
+          </div>
+          <InputMoment
+            moment={this.state.m}
+            onChange={this.handleChange}
+            onSave={this.handleSave}
           />
-        </div>
-        <InputMoment
-          moment={this.state.m}
-          onChange={this.handleChange}
-          onSave={this.handleSave}
-        />
         </form>
       </div>
     );
-  },
-
-  handleChange(m) {
-    this.setState({ m });
-  },
-
-  handleSave() {
-    console.log('saved', this.state.m.format('llll'));
   }
-});
+}
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
