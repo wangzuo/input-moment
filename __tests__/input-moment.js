3a9e1644d93ef3ec207382d1858b970b359f8723
Wang Zuo <wzuoadjusted@gmail.com>
2017-06-05 10:34:00 +0800

    fix test

--- __tests__/input-moment.js
+++ __tests__/input-moment.js
@@ -4,7 +4,7 @@ import renderer from 'react-test-renderer';
 import InputMoment from '../src/input-moment';
 
 test('render', () => {
-  const m = moment().year(2018).hours(8).minutes(20).seconds(0);
+  const m = moment().year(2018).month(7).date(8).hours(8).minutes(8).seconds(8);
   const component = <InputMoment moment={m} />;
   expect(renderer.create(component).toJSON()).toMatchSnapshot();
 });
